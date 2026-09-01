1bb012726c5498cf3df1996cb1231382e2d10888
Angel Rodriguez <angel26mito30@gmail.com>
2026-09-01 11:35:49 -0500

    Creamos la clase madre vehiculos y la primera subclase carros

---
+++ src/main/java/clasedeobjetos/consecionario/Carros.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clasedeobjetos.consecionario;
+
+/**
+ *
+ * @author angel
+ */
+public class Carros {
+    
+    private int numerosPuertas = 0;
+    private String tipoGasolina = "";
+    private double cilindraje = 0;
+    private boolean automatico = false;
+    
+}
