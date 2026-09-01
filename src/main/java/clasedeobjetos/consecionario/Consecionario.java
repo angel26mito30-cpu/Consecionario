b8c05f7fd886c0b1431a84e5af11433b0fa4232a
Angel Rodriguez <angel26mito30@gmail.com>
2026-09-01 10:58:32 -0500

    Inicio del proyecto Consecionario

---
+++ src/main/java/clasedeobjetos/consecionario/Consecionario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package clasedeobjetos.consecionario;
+
+/**
+ *
+ * @author angel
+ */
+public class Consecionario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
