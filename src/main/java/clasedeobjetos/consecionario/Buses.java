b896585cd875ebbdfabadd2848d15586587f9508
Angel Rodriguez <angel26mito30@gmail.com>
2026-09-02 18:41:38 -0500

    agregamos los constructores en la clase buses

--- src/main/java/clasedeobjetos/consecionario/Buses.java
+++ src/main/java/clasedeobjetos/consecionario/Buses.java
@@ -8,10 +8,21 @@ package clasedeobjetos.consecionario;
  *
  * @author angel
  */
-public class Buses {
+public class Buses extends Vehiculos {
     
     private int numeroPasajeros = 0;
     private String tipoServicio = "";
     protected double velMax = 0;
-    private boolean wifi = true;        
+    private boolean wifi = true;  
+    
+    public Buses (String marca, int año, double precio, boolean disponible, 
+           int numeroPasajeros, String tipoServicio, double velMax, boolean wifi){
+    
+           super(marca, año, precio, disponible);
+            this.numeroPasajeros = numeroPasajeros;
+            this.tipoServicio = tipoServicio;
+            this.velMax = velMax;
+            this.wifi = wifi;
+            
+    }
 }
